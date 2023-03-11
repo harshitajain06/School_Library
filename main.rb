6a0bf884748ed021da3d9c14b9511c0fdfad7a07
Harshita Jain <harshitaforever06@gmail.com>
2023-03-11 23:01:18 +0530

    linters corrected

--- main.rb
+++ main.rb
@@ -3,31 +3,15 @@ require_relative 'app'
 def main
   app = App.new
   loop do
-    list_of_options
+    display_menu_options
     choice = gets.chomp.to_i
-    case choice
-    when 1
-      app.list_books
-    when 2
-      app.list_people
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.create_rental
-    when 6
-      app.list_rentals_by_person_id
-    when 7
-      puts 'Exiting the Library Management System. Goodbye!'
-      break
-    else
-      puts 'Invalid choice. Please choose again.'
-    end
+    handle_menu_choice(choice, app)
+    break if choice == 7
   end
+  puts 'Exiting the Library Management System. Goodbye!'
 end
 
-def list_of_options
+def display_menu_options
   puts "\nWhat would you like to do?"
   puts '1. List all books'
   puts '2. List all people'
@@ -37,4 +21,24 @@ def list_of_options
   puts '6. List all rentals for a given person id'
   puts '7. Quit'
 end
+
+def handle_menu_choice(choice, app)
+  case choice
+  when 1
+    app.list_books
+  when 2
+    app.list_people
+  when 3
+    app.create_person
+  when 4
+    app.create_book
+  when 5
+    app.create_rental
+  when 6
+    app.list_rentals_by_person_id
+  else
+    puts 'Invalid choice. Please choose again.'
+  end
+end
+
 main
