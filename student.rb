c7c2b31c833910ae4a53e9c299974cc8bae7a489
Jonah Wambua <djmrjay@gmail.com>
2023-03-15 17:52:26 +0300

    Add parent_permission attribute

--- student.rb
+++ student.rb
@@ -1,10 +1,13 @@
 require_relative 'person'
-class Student < Person
-  attr_accessor :classroom
 
-  def initialize(age, classroom, name, parent_permission: true)
+class Student < Person
+  attr_accessor :classroom, :parent_permission
+  attr_reader :json_class
+
+  def initialize(age, classroom, name, parent_permission: true, json_class: 'Student')
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @json_class = json_class
   end
 
   def play_hooky
