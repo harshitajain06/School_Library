96fbd40375c90d73618cf3fd7f10cd41195215e4
Jonah Wambua <djmrjay@gmail.com>
2023-03-15 17:53:18 +0300

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,11 +1,13 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_reader :specialization
+  attr_accessor :specialization
+  attr_reader :json_class
 
-  def initialize(age, specialization, name, parent_permission: true)
+  def initialize(age, specialization, name, parent_permission: true, json_class: 'Teacher')
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
+    @json_class = json_class
   end
 
   def can_use_services?
